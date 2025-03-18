7f9cbe4d78ad32d2d4ac6c80e8ee5e7f379943ff
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-03-18 17:13:07 +0800

    Terakhir Melakukan Case 3 yaitu membuat pilihan and salah kalau selain pilihan yang berada di opsi

--- Pertemuan 4 part 3.cpp
+++ Pertemuan 4 part 3.cpp
@@ -20,24 +20,34 @@ int main() {
         cout << "3. Exit" << endl;
         cout << "Pilihan (1/2/3) : ";
         cin >> pilihan;
+
         switch (pilihan) {
-            case 1:
-                cout << "Menghitung Luas Persegi Panjang" << endl;
-                cout << "Masukkan Panjang : ";
-                cin >> panjang;
-                cout << "Masukkan Lebar : ";
-                cin >> lebar;
-                cout << "Luas Persegi Panjang : "
-                     << LuasPersegiPanjang(panjang, lebar) << endl;
-                break;
-                case 2:
-                cout << "Menghitung Luas Lingkaran" << endl;
-                cout << "Masukkan Jejari : ";
-                cin >> jejari;
-                cout << "Luas Lingkaran : "
-                     << LuasLingkaran(jejari) << endl;
-                cout << endl;
-                break;
-    
-}
+        case 1:
+            cout << "Menghitung Luas Persegi Panjang" << endl;
+            cout << "Masukkan Panjang : ";
+            cin >> panjang;
+            cout << "Masukkan Lebar : ";
+            cin >> lebar;
+            cout << "Luas Persegi Panjang : "
+                 << LuasPersegiPanjang(panjang, lebar) << endl;
+            break;
+
+        case 2:
+            cout << "Menghitung Luas Lingkaran" << endl;
+            cout << "Masukkan Jejari : ";
+            cin >> jejari;
+            cout << "Luas Lingkaran : "
+                 << LuasLingkaran(jejari) << endl;
+            cout << endl;
+            break;
+
+        case 3:
+            break;
+
+        default:
+            cout << "Pilihan anda salah!" << endl;
+            cout << endl;
+            break;
+        }
+    } while (pilihan != 3);
 }
